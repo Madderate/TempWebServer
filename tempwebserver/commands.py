d95cce30f6ab540def57f42692f2b997e08f63ba
Jianzengzhi Liao <liaojianzengzhi@hotmail.com>
2020-06-02 12:05:28 +0800

    use random

--- tempwebserver/commands.py
+++ tempwebserver/commands.py
@@ -1,48 +1,47 @@
 from tempwebserver import app
 import json
-
-user = {'name': 'madderate'}
-environment = {
-    'temperature': 24,
-    'humidity': 50,
-    'luminous_intensity': 500,
-    'air_quality': 25
-}
-temperature = {'temperature': 24}
-humidity = {'humidity': 50}
-luminousIntensity = {'luminous_intensity': 500}
-airQuality = {'air_quality': 25}
+import random
 
 
 # 处理请求
 @app.route('/')
 def index():
     # return render_template('index.html', environmentQuery=environmentQuery)
-
     # return json.dumps(environmentQuery, ensure_ascii=False)
+    user = {'name': 'madderate'}
     return json.dumps(user)
 
 
 @app.route('/environment')
 def get_environment():
+    environment = {
+        'temperature': random.randint(20, 23),
+        'humidity': random.randint(12, 100),
+        'luminous_intensity': random.randint(200, 1600),
+        'air_quality': random.randint(5, 200)
+    }
     return json.dumps(environment)
 
 
 @app.route('/temperature')
 def get_temperature():
+    temperature = {'temperature': random.randint(20, 23)}
     return json.dumps(temperature)
 
 
 @app.route('/humidity')
 def get_humidity():
+    humidity = {'humidity': random.randint(12, 100)}
     return json.dumps(humidity)
 
 
 @app.route('/luminous_intensity')
 def get_luminous_intensity():
+    luminousIntensity = {'luminous_intensity': random.randint(200, 1600)}
     return json.dumps(luminousIntensity)
 
 
 @app.route('/air_quality')
 def get_air_quality():
+    airQuality = {'air_quality': random.randint(5, 200)}
     return json.dumps(airQuality)
