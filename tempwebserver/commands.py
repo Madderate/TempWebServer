f8e0cb1477b6d7eef7eb7a4908e309e39bf3c41f
Jianzengzhi Liao <liaojianzengzhi@hotmail.com>
2020-06-02 10:32:59 +0800

    Add some route functions

--- tempwebserver/commands.py
+++ tempwebserver/commands.py
@@ -1,13 +1,18 @@
 from tempwebserver import app
 import json
 
-name = 'madderate'
-environmentList = {
+user = {'name': 'madderate'}
+environment = {
     'temperature': 24,
     'humidity': 50,
     'luminous_intensity': 500,
     'air_quality': 25
 }
+temperature = {'temperature': 24}
+humidity = {'humidity': 50}
+luminousIntensity = {'luminous_intensity': 500}
+airQuality = {'air_quality': 25}
+
 
 # 处理请求
 @app.route('/')
@@ -15,4 +20,29 @@ def index():
     # return render_template('index.html', environmentQuery=environmentQuery)
 
     # return json.dumps(environmentQuery, ensure_ascii=False)
-    return json.dumps(environmentList)
+    return json.dumps(user)
+
+
+@app.route('/environment')
+def get_environment():
+    return json.dumps(environment)
+
+
+@app.route('/temperature')
+def get_temperature():
+    return json.dumps(temperature)
+
+
+@app.route('/humidity')
+def get_humidity():
+    return json.dumps(humidity)
+
+
+@app.route('/luminous_intensity')
+def get_luminous_intensity():
+    return json.dumps(luminousIntensity)
+
+
+@app.route('/air_quality')
+def get_air_quality():
+    return json.dumps(airQuality)
